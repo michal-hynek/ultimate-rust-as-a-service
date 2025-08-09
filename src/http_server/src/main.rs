e433cdda9d257efb456f48819fe024e4f1b6446c
michal-hynek <hynek.michal@gmail.com>
2025-08-09 11:07:22 -0700

    Path, query, and header extractors

--- src/http_server/src/main.rs
+++ src/http_server/src/main.rs
@@ -1,13 +1,31 @@
-use axum::{response::Html, routing::get, Router};
+use std::collections::HashMap;
+
+use axum::{extract::{Path, Query}, response::Html, routing::get, Router};
+use axum::http::HeaderMap;
 
 async fn test() -> Html<&'static str> {
     Html("<h1>Hello world<h1>")
 }
 
+async fn path_extract(Path(id): Path<u32>) -> Html<String> {
+    Html(format!("Book ID {id}"))
+}
+
+async fn query_extract(Query(params): Query<HashMap<String, String>>) -> Html<String> {
+    Html(format!("{params:#?}"))
+}
+
+async fn header_extract(headers: HeaderMap) -> Html<String> {
+    Html(format!("{headers:#?}"))
+}
+
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(test));
+        .route("/", get(test))
+        .route("/books/{id}", get(path_extract))
+        .route("/books", get(query_extract))
+        .route("/headers", get(header_extract));
 
     let addr = "127.0.0.1:3001";
     let listener = tokio::net::TcpListener::bind(addr)
